22e9cd91e8dffea4a4331d26d7f5b2a0792a50bb
Taha Elkarroumy <elkarroumytaha@gmail.com>
2023-03-05 14:13:32 +0000

    init

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'ckeditor', # CKEditor config
     'ckeditor_uploader',
     'django.contrib.humanize',
+    'request',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'request.middleware.RequestMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
